e910850128fc8b32b494581ab252f5a6091ef0e6
HuangJiaTong <1641505792@qq.com>
2020-09-28 23:28:11 +0800

    lab1添加hjtDialogActivity.java

---
+++ AndroidLab_1/hjtAndroidLab_1/app/src/main/java/com/example/hjtandroidlab_1/hjtDialogActivity.java
@@ -0,0 +1,14 @@
+package com.example.hjtandroidlab_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class hjtDialogActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hjt_dialog);
+    }
+}
