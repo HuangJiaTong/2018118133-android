42836f8fcfbc4df59280e800760599a7e43ad0d6
HuangJiaTong <1641505792@qq.com>
2020-10-07 17:00:46 +0800

    控件练习

--- AndroidLab_3/hjtAndroidLab_3/app/src/main/java/com/example/hjtandroidlab_3/MainActivity.java
+++ AndroidLab_3/hjtAndroidLab_3/app/src/main/java/com/example/hjtandroidlab_3/MainActivity.java
@@ -3,12 +3,38 @@ package com.example.hjtandroidlab_3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private EditText editText;
+
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.button);
+        editText = (EditText) findViewById(R.id.edit_text);
+        imageView = (ImageView) findViewById(R.id.image_view);
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.button:
+                String inputText = editText.getText().toString();
+                Toast.makeText(MainActivity.this,inputText,Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                break;
+        }
+
     }
 }
