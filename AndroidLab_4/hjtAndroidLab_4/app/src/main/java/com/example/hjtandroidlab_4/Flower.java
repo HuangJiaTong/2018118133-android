b3bf4beb84467bd0c6e331d1306f74ceca2dee65
HuangJiaTong <1641505792@qq.com>
2020-10-14 15:04:28 +0800

    添加Flower.java

--- AndroidLab_4/hjtAndroidLab_4/app/src/main/java/com/example/hjtandroidlab_4/Flower.java
+++ AndroidLab_4/hjtAndroidLab_4/app/src/main/java/com/example/hjtandroidlab_4/Flower.java
@@ -1,4 +1,17 @@
 package com.example.hjtandroidlab_4;
 
 public class Flower {
+    private String name;
+    private int imageId;
+    public Flower(String name,int imageId){
+        this.name=name;
+        this.imageId=imageId;
+    }
+    public String getName(){
+        return name;
+    }
+    public int getImageId(){
+        return imageId;
+    }
 }
+
