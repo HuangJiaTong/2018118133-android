04a4583fbbe6b3971060b47f4a1967a9973eadb7
HuangJiaTong <1641505792@qq.com>
2020-10-14 15:10:55 +0800

    添加项目所需图片

--- AndroidLab_3/hjtAndroidLab_3/app/src/main/java/com/example/hjtandroidlab_3/MainActivity.java
+++ AndroidLab_3/hjtAndroidLab_3/app/src/main/java/com/example/hjtandroidlab_3/MainActivity.java
@@ -1,14 +1,10 @@
 package com.example.hjtandroidlab_3;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
