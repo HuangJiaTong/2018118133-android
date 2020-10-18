603e1798716ef2581b199abcba01975ec41fe9fb
HuangJiaTong <1641505792@qq.com>
2020-10-18 10:16:54 +0800

    添加四个按钮

--- AndroidLab_3/hjtAndroidLab_3/app/src/main/java/com/example/hjtandroidlab_3/MainActivity.java
+++ AndroidLab_3/hjtAndroidLab_3/app/src/main/java/com/example/hjtandroidlab_3/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.hjtandroidlab_3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button1 = (Button) findViewById(R.id.button1);
+        Button button2 = (Button) findViewById(R.id.button2);
+        Button button3 = (Button) findViewById(R.id.button3);
+        Button button4 = (Button) findViewById(R.id.button4);
+
     }
 }
