8ed2701fc8f0cc5d69b7dddd8e0417d805d997a2
HuangJiaTong <1641505792@qq.com>
2020-10-18 16:29:20 +0800

    更新EditActivity

--- AndroidLab_3/hjtAndroidLab_3/app/src/main/java/com/example/hjtandroidlab_3/EditActivity.java
+++ AndroidLab_3/hjtAndroidLab_3/app/src/main/java/com/example/hjtandroidlab_3/EditActivity.java
@@ -3,6 +3,7 @@ package com.example.hjtandroidlab_3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class EditActivity extends AppCompatActivity {
 
@@ -10,5 +11,6 @@ public class EditActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit);
+        Button button = (Button) findViewById(R.id.button1);
     }
 }
