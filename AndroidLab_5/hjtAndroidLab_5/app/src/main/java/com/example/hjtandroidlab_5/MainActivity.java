0a450665014d5822bc51d5baae2b298b0ebed323
HuangJiaTong <1641505792@qq.com>
2020-10-20 15:25:27 +0800

    发送自定义标准广播

--- AndroidLab_5/hjtAndroidLab_5/app/src/main/java/com/example/hjtandroidlab_5/MainActivity.java
+++ AndroidLab_5/hjtAndroidLab_5/app/src/main/java/com/example/hjtandroidlab_5/MainActivity.java
@@ -3,12 +3,15 @@ package com.example.hjtandroidlab_5;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.BroadcastReceiver;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -25,6 +28,16 @@ public class MainActivity extends AppCompatActivity {
         intentFilter.addAction("android.net.conn.CONNECTIVITY_CHANGE");
         networkChangeReceiver = new NetworkChangeReceiver();
         registerReceiver(networkChangeReceiver,intentFilter);
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new
+                        Intent("com.example.broadcasttest.MY_BROADCAST");
+                intent.setComponent(new ComponentName(getPackageName(),"com.example.hjtandroidlab_5.MyBroadcastReceiver"));
+                sendBroadcast(intent);
+            }
+        });
         }
     @Override
     protected void onDestroy(){
