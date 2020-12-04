91f7c74e7161e38043ffe9077fe2bcb9c5e2d764
HuangJiaTong <1641505792@qq.com>
2020-12-04 10:25:00 +0800

    修改MainActivity

--- AndroidLab_9/AsyncTaskTest/app/src/main/java/com/example/asynctasktest/MainActivity.java
+++ AndroidLab_9/AsyncTaskTest/app/src/main/java/com/example/asynctasktest/MainActivity.java
@@ -2,13 +2,98 @@ package com.example.asynctasktest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    MyTask mTask;
+    Button button,cancel; // 加载、取消按钮
+    TextView text; // 更新的UI组件
+    ProgressBar progressBar; // 进度条
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+        cancel = findViewById(R.id.cancel);
+        text = findViewById(R.id.text);
+        progressBar = findViewById(R.id.progress_bar);
+        mTask = new MyTask();
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mTask.execute();
+            }
+        });
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mTask.cancel(true);
+            }
+        });
+    }
+    private class MyTask extends AsyncTask<String, Integer, String> {
+
+        // 方法1：onPreExecute（）
+        // 作用：执行线程任务前的操作
+        @Override
+        protected void onPreExecute() {
+            text.setText("加载中");
+            // 执行前显示提示
+        }
+
+
+        // 方法2：doInBackground（）
+        // 作用：接收输入参数、执行任务中的耗时操作、返回 线程任务执行的结果
+        // 此处通过计算从而模拟“加载进度”的情况
+        //在子线程中运行
+        @Override
+        protected String doInBackground(String... params) {
+
+            try {
+                int count = 0;
+                int length = 1;
+                while (count<99) {
+
+                    count += length;
+                    // 可调用publishProgress（）显示进度, 之后将执行onProgressUpdate（）
+                    publishProgress(count); //此方法自动调用onProgressUpdate()来执行
+                    // 模拟耗时任务
+                    Thread.sleep(50);
+                }
+            }catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        // 方法3：onProgressUpdate（）
+        // 作用：在主线程显示线程任务执行的进度
+        @Override
+        protected void onProgressUpdate(Integer... progresses) {
+            progressBar.setProgress(progresses[0]);
+            text.setText("loading..." + progresses[0] + "%");
+        }
+
+        // 方法4：onPostExecute（）
+        // 作用：接收线程任务执行结果、将执行结果显示到UI组件
+        @Override
+        protected void onPostExecute(String result) {
+            // 执行完毕后，则更新UI
+            text.setText("加载完毕");
+        }
+        // 方法5：onCancelled()
+        // 作用：将异步任务设置为：取消状态
+        //当调用cancel()系统会调用onCancelled()方法。
+        @Override
+        protected void onCancelled() {
+            text.setText("已取消");
+            progressBar.setProgress(0);
+        }
     }
 }
