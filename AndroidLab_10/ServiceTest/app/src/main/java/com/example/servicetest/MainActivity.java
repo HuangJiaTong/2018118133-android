ff8b9fec3918871257362a2611341fa4c7ae47ec
HuangJiaTong <1641505792@qq.com>
2020-12-13 13:47:28 +0800

    修改MainActivity打印主线程和子线程的ID

--- AndroidLab_10/ServiceTest/app/src/main/java/com/example/servicetest/MainActivity.java
+++ AndroidLab_10/ServiceTest/app/src/main/java/com/example/servicetest/MainActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.content.ServiceConnection;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -41,6 +42,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Button unbindService = (Button) findViewById(R.id.unbind_service);
         bindService.setOnClickListener(this);
         unbindService.setOnClickListener(this);
+        Button startIntentService  = (Button) findViewById(R.id.start_intent_service);
+        startIntentService.setOnClickListener(this);
     }
 
     @Override
@@ -60,6 +63,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
             case R.id.unbind_service:
                 unbindService(connection);//解绑服务
+                break;
+            case R.id.start_intent_service:
+                //打印主线程的ID
+                Log.d("MainActivity","Thread id is" + Thread.currentThread().getId());
+                Intent intentService = new Intent(this,MyIntentService.class);
+                startService(intentService);
                 break;
             default:
                 break;
