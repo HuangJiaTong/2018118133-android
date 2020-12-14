6e6cf81a62289d756dc46c0031674ceae99487ca
HuangJiaTong <1641505792@qq.com>
2020-12-14 16:19:44 +0800

    修改MainActivity中的代码

--- AndroidLab_12/Material_Design/app/src/main/java/com/example/material_design/MainActivity.java
+++ AndroidLab_12/Material_Design/app/src/main/java/com/example/material_design/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.material_design;
 
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
 
 import android.os.Bundle;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 }
