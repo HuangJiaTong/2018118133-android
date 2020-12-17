1341b278186f5524d5254e5eb7688fe2d0b97cb0
HuangJiaTong <1641505792@qq.com>
2020-12-17 22:56:31 +0800

    判断点击事件，主界面完成

--- curriculum_design/Healthy_diet/app/src/main/java/com/example/healthy_diet/HomeMenuActivity.java
+++ curriculum_design/Healthy_diet/app/src/main/java/com/example/healthy_diet/HomeMenuActivity.java
@@ -3,6 +3,7 @@ package com.example.healthy_diet;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class HomeMenuActivity extends AppCompatActivity {
 
@@ -10,5 +11,21 @@ public class HomeMenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_menu);
+    }
+
+    public void onClick(View view) {
+
+        switch (view.getId()) {
+            case R.id.home_btn1:
+
+                break;
+            case R.id.home_btn2:
+
+                break;
+            case R.id.home_btn3:
+                
+                break;
+        }
+
     }
 }
