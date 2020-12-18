dbe3bfdb64fcdd8d21e9d4f1956ea618e0ebb328
HuangJiaTong <1641505792@qq.com>
2020-12-18 22:23:03 +0800

    新建一个food_grid包，在其中添加FoodGridActivity活动

---
+++ curriculum_design/Healthy_diet/app/src/main/java/com/example/healthy_diet/food_grid/FoodGridActivity.java
@@ -0,0 +1,16 @@
+package com.example.healthy_diet.food_grid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.healthy_diet.R;
+
+public class FoodGridActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_food_grid);
+    }
+}
