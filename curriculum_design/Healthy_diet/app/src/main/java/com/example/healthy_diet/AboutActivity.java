f968188dafabe7b5274ea95d544b2dceb86000b3
HuangJiaTong <1641505792@qq.com>
2020-12-19 13:42:02 +0800

    创建图片指示器，设置ViewPager页面的监听器

--- curriculum_design/Healthy_diet/app/src/main/java/com/example/healthy_diet/AboutActivity.java
+++ curriculum_design/Healthy_diet/app/src/main/java/com/example/healthy_diet/AboutActivity.java
@@ -49,20 +49,54 @@ public class AboutActivity extends AppCompatActivity implements View.OnClickList
         pointLayout = findViewById(R.id.about_layout_point);
         shareTv.setOnClickListener(this);
         viewList = new ArrayList<>();
+        pointList = new ArrayList<>();
         //初始化ViewPager的页面信息
         for (int i = 0; i < picIds.length; i++) {
             View view = LayoutInflater.from(this).inflate(R.layout.item_aboutvp, null);
             ImageView iv = view.findViewById(R.id.item_aboutvp_iv);
             iv.setImageResource(picIds[i]);
             viewList.add(view);
+            //创建指示器内容
+            ImageView pointIv = new ImageView(this);
+            //在代码中设置控件的宽高和外边距等属性
+            LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+            lp.setMargins(0,0,20,0);
+            //将布局参数设置给ImageView
+            pointIv.setLayoutParams(lp);
+            pointIv.setImageResource(R.mipmap.a2);
+            pointList.add(pointIv); //添加到集合当中便于统一管理
+            pointLayout.addView(pointIv); //添加到布局当中显示出来
         }
+        //设置第一个小圆点为选中状态
+        pointList.get(0).setImageResource(R.mipmap.a3);
         // 创建适配器对象
         adapter = new AboutAdapter(viewList);
         //设置适配器
         aboutVp.setAdapter(adapter);
         //发送切换页面消息
         handler.sendEmptyMessageDelayed(1,5000);
+        //设置ViewPager页面监听器
+        setVPListener();
 
+    }
+
+    private void setVPListener() {
+        //设置ViewPager的监听器
+        aboutVp.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+            }
+            @Override
+            public void onPageSelected(int position) {
+                for (int i = 0; i < pointList.size(); i++) {
+                    pointList.get(i).setImageResource(R.mipmap.a2);
+                }
+                pointList.get(position%pointList.size()).setImageResource(R.mipmap.a3);
+            }
+            @Override
+            public void onPageScrollStateChanged(int state) {
+            }
+        });
     }
 
     @Override
